05acd4b85145c6e3bed7618835aa0b03fea62d43
Svet-lik <svet-st@yandex.ru>
2021-03-19 16:31:06 +0300

    Day 4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+const modal = document.querySelector('.modal');
+
+modal.addEventListener('submit', evt => {
+  evt.preventDefault();
+  const btnClose = document.querySelector('.fancybox-button.fancybox-close-small');
+  btnClose.click();
+});
+
 $(document).ready(function () {  
   $('#app').vide({
     mp4: 'video/video.mp4',
